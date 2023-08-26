11f717218873b0009375bdae7d1991468828126c
quent-coders <135216173+quent-coders@users.noreply.github.com>
2023-08-26 16:32:20 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const overall = {'period': 'sep 2, 2022 - may 26, 2023 (inclusive)',
+const overall = {'period': 'sep 2, 2022 - may 26, 2023',
                 'total_study_hours': '58.5'}
 const topics = ['Introduction to OOP - {theory: 0:40, practice: 1:30}',
                 'Classes - {theory: 0:40, practice: 1:30}',
@@ -16,23 +16,21 @@ const topics = ['Introduction to OOP - {theory: 0:40, practice: 1:30}',
                 'Lists In-Depth - {theory: 2:30, practice: 6:00}',
                 'Recursion, Application in Python - {theory: 0:40, practice: 1:30}']
 
-        //     'projects': {
-        //         'coffee-machine': {
-        //             'difficulty': '3/10',
-        //             'topics requires': '40+',
-        //             'status': 'complete - 6/6 stages',
-        //             'project time avg.': '8+ hrs',
-        //             'implementation': 'class'
-        //         },
-        //         'domino': {
-        //           'difficulty': '5/10',
-        //           'topics required': '60+',
-        //           'status': 'in progress - 2/5 stages',
-        //           'project time avg.': '10+ hrs',
-        //           'implementation': 'class'
-        //         }
-        //     }
-        // }
+const projects = ['project name - coffee-maker (backend)',
+                    'project difficulty - 3/10',
+                    'topics required to work - 40+',
+                    'project work time est. - 9+ hrs',
+                    'project implementation - class',
+                    'project status - complete (6/6 stages)',
+
+                    'project name - domino (backend)',
+                    'project difficulty - 5/10',
+                    'topics required to work - 60+',
+                    'project time est. - 11+ hrs',
+                    'implementation - class',
+                    'status - complete (5/5 stages)'
+                ]
+         
 
 let overall_dom = document.querySelector('.overall');
 for (const [k, v] of Object.entries(overall)) {
@@ -52,4 +50,20 @@ for (const topic of topics) {
   let p = document.createElement("p");
   div.append(`${topic.split(' - ')[0]}: ${topic.split(' - ')[1]}`, p);
   document.querySelector('.content-1').appendChild(div);
+};
+
+let projects_dom = document.querySelector('.projects');
+let n = 0;
+for (const project of projects) {
+  let div = document.createElement("div");
+  div.classList.add("box")
+  if (!(n % 6)) {
+    div.classList.add("project-name-box");
+  }
+  
+  let p = document.createElement("p");
+  div.append(`${project.split(' - ')[0]}: ${project.split(' - ')[1]}`, p);
+  document.querySelector('.content-2').appendChild(div);
+
+  n++;
 };
